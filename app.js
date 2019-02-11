f9f11b143c160d4ce3f10aff5793322a163df7d9
Freeptom <32514405+Freeptom@users.noreply.github.com>
2019-02-11 21:00:31 +0000

    added partials

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.set('view engine', "ejs");
+
+
+
 app.get('/', function(req, res){
-    res.send('hi there');
+    res.render("home");
 });
-app.get('/bye', function(req, res){
-    res.send('bye');
+
+
+app.get('/fallinlovewith/:thing', function(req, res) {
+    let thing = req.params.thing;
+    res.render('love', {thingVar: thing});
 });
+
+app.get("/posts", function(req,res) {
+    let posts = [
+        {title: "post 1", author: "Suzie"},
+        {title: "My pet bunny", author: "John"},
+        {title: "Can you believe this?!", author: "Betty"}
+    ];
+
+    res.render("posts", {posts: posts})
+});
+
 
 app.get('*', function(req, res){
     res.send("404 m8");
-})
+});
 
 
 app.listen(3000, function(){
     console.log('port is listening')
-})
+}) 
